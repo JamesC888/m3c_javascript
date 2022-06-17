203e6b709a4147763929f814bd8c471e8c76ed34
JamesC888 <jchmilowsky@gmail.com>
2022-06-17 13:40:36 -0700

    fixed alert messages

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+
+var upperArray = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var lowerArray = "qwertyuiopasdfghjklzxcvbnm"
+var numberArray = "123456789"
+var specialArray = "!@#$&()<>"
 
 
-// ----generate a prompt        prompt()
-// ----prompt will ask length of pw 8-128
-// ----prompts for upper/lower/numbers/special]
-//  -?-user must select at least 1 prompt
-// -?- return info and send to generate pw
 
-// var characters = ["qwertyuiopasdfghjklzxcvbnm", "QWERTYUIOPASDFGHJKLZXCVBNM", "123456789", "!@#$&()<>"]
-// var expression1 = (length != "number");
-// var expression2 = (length > 128);
-// var expression3 = (length < 8);
 
-function passwordPrompt() {
-     var length = prompt("How many characters? \nMin 8 Max 128", "enter here");
 
-       if (length < 8 && length > 128) {
-        alert("Invalid Response");
-        //passwordPrompt()
-      };    // typeof number
+function generatePassword() {
+   var length = prompt("How many characters? \nMin 8 Max 128", "enter here")   
+     if (length < 8 || length > 128 ){
+     alert("invalid response");
+     return;
+     }        
+ 
 
-       
-         var lower = confirm("lowercase characters?");
-         var upper = confirm("UPPERCASE CHARACTERS?");
-         var numeric = confirm("123456789?");
-         var special = confirm("!@#$&()<>");
+     var lower = confirm("lowercase characters? \nqwerty");
+     var upper = confirm("UPPERCASE CHARACTERS? \nQWERTY");
+     var numeric = confirm("123456789?");
+     var special = confirm("!@#$&()<>");
+
+     if (!lower && !upper && !numeric && !special) {
+         alert("invalid response");
+         return;
+     }
+    
+
+     if (lower) {
+        lower = true
+
+     }
+     
+     if (upper) {
+        upper = true
+
+     }
+
+     if(numeric) {
+        numeric = true
+
+     }
+
+     if (special) {
+        special = true
+
+     }
+
         // var create = createPassword(lower, upper, numeric, special)  
-  };
- //length x create
-passwordPrompt()
+};
 
 
+generatePassword()
 
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
