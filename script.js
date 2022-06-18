6348687426f78878f972d32c1431203f2f9b2a5c
JamesC888 <jchmilowsky@gmail.com>
2022-06-17 18:17:08 -0700

    generator working

--- script.js
+++ script.js
@@ -1,81 +1,69 @@
-
-var upperArray = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var lowerArray = "qwertyuiopasdfghjklzxcvbnm"
-var numberArray = "123456789"
-var specialArray = "!@#$&()<>"
-var passwordLength = 8
-
-var createPassword = ""
-var password = ""
+var password = document.getElementById("password");
 
 
+
+var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-   var passwordLength = prompt("How many characters? \nMin 8 Max 128", "enter here")   
-     if (passwordLength < 8 || passwordLength > 128 ){
-     alert("invalid response");
-     return;
-     }        
- 
-
-     var lower = confirm("lowercase characters? \nqwerty");
-     var upper = confirm("UPPERCASE CHARACTERS? \nQWERTY");
-     var numeric = confirm("123456789?");
-     var special = confirm("!@#$&()<>");
-
-     
-     if (!lower && !upper && !numeric && !special) {
-         alert("invalid response");
-         return;
-     };
-    
-
-     if (lower) {
-        createPassword += lowerArray;
-
-     };
-     
-     if (upper) {
-        creatPassword += upperArray;
-
-     };
-
-     if(numeric) {
-        createPassowrd += numberArray;
-
-     };
-
-     if (special) {
-        createPassword += specialArray;
-
-     };
-
-    for (i = 0; i < length; i++) {
-      var randomPass = Math.floor(Math.random() * passwordLength.length);
-      var genPass = createPassword[randomPass, randomPass +1];
-      password = genPass;
+   var lowerArray = "qwertyuiopasdfghjklzxcvbnm";
+   var upperArray = "QWERTYUIOPASDFGHJKLZXCVBNM";
+   var numericArray = "123456789";
+   var specialArray = "!@#$&()<>";
+   
+   
+      var password = "";
+      var create = "";
       
-   };  
-      return;
+      var passwordLength = prompt("How many characters? \nMin 8 Max 128", "enter here");   
+      if (passwordLength < 8 || passwordLength > 128 ) {
+      alert("invalid response");
+      
+      };  
+   
+      var lower = confirm("lowercase characters? \nqwerty");
+      var upper = confirm("UPPERCASE CHARACTERS? \nQWERTY");
+      var numeric = confirm("123456789?");
+      var special = confirm("!@#$&()<>");
+      
+      if (!lower && !upper && !numeric && !special) {
+            alert("invalid response");
+            
+      };
+      if (lower) {
+         create+=lowerArray;
+      };      
+      if (upper) {
+         create+=upperArray;
 
+      };
+      if(numeric) {
+         create+=numericArray;
+      };
+
+      if (special) {
+         create+=specialArray;
+      };
+               
+
+      for ( i = 0; i < passwordLength; i++) {
+         var randomPass = Math.floor(Math.random() * create.length);
+         var choice = create.substring(randomPass, randomPass +1);
+         password+=choice
+      };  return password
    };
-
-document.getElementById("password")
-
-
-
-// // Assignment Code
- var generateBtn = document.querySelector("#generate");
-
-
-// // Write password to the #password input
- function writePassword() {
-   var password = generatePassword();
-   var passwordText = document.querySelector("#password");
-
-   passwordText.value = password;
-
- }
-
-// // Add event listener to generate button
- generateBtn.addEventListener("click", writePassword);
+   
+   
+   
+   
+   
+   
+   function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      
+      
+      passwordText.value = password;
+      
+   }
+   
+generateBtn.addEventListener("click", writePassword);
 
