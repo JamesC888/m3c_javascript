f4e553b4cdfd556a7351dea9c5740d4ff9b0e4f1
JamesC888 <jchmilowsky@gmail.com>
2022-06-20 10:52:07 -0700

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # m3c_javascript
-password generator
+This is a password generator I built. It takes multiple paramters set by the user and outputs a password within those specs. It uses a full alphabet(upper and lowercase), numbers(1-9, i excluded 0 by choice) and a few special characters.
+
+![Generator Screenshot](./JCPG.png)
+
+[Check it out here!]()
--- script.js
+++ script.js
@@ -1,69 +1,57 @@
 var password = document.getElementById("password");
 
-
-
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
    var lowerArray = "qwertyuiopasdfghjklzxcvbnm";
    var upperArray = "QWERTYUIOPASDFGHJKLZXCVBNM";
    var numericArray = "123456789";
    var specialArray = "!@#$&()<>";
-   
-   
-      var password = "";
-      var create = "";
       
-      var passwordLength = prompt("How many characters? \nMin 8 Max 128", "enter here");   
-      if (passwordLength < 8 || passwordLength > 128 ) {
+   var password = "";
+   var create = "";
+      
+   var passwordLength = prompt("How many characters? \nMin 8 Max 128", "enter here");   
+   if (passwordLength < 8 || passwordLength > 128 ) {
       alert("invalid response");
       
-      };  
+   };  
    
-      var lower = confirm("lowercase characters? \nqwerty");
-      var upper = confirm("UPPERCASE CHARACTERS? \nQWERTY");
-      var numeric = confirm("123456789?");
-      var special = confirm("!@#$&()<>");
+   var lower = confirm("lowercase characters? \nqwerty");
+   var upper = confirm("UPPERCASE CHARACTERS? \nQWERTY");
+   var numeric = confirm("Numbers? \n123456789");
+   var special = confirm("Special characters? \n!@#$&()<>");
       
-      if (!lower && !upper && !numeric && !special) {
-            alert("invalid response");
+   if (!lower && !upper && !numeric && !special) {
+      alert("invalid response");
             
-      };
-      if (lower) {
-         create+=lowerArray;
-      };      
-      if (upper) {
-         create+=upperArray;
-
-      };
-      if(numeric) {
-         create+=numericArray;
-      };
-
-      if (special) {
-         create+=specialArray;
-      };
-               
-
-      for ( i = 0; i < passwordLength; i++) {
-         var randomPass = Math.floor(Math.random() * create.length);
-         var choice = create.substring(randomPass, randomPass +1);
-         password+=choice
-      };  return password
    };
+   if (lower) {
+      create += lowerArray;
+   };      
+   if (upper) {
+      create += upperArray;
+   };
+   if(numeric) {
+      create += numericArray;
+   };
+   if (special) {
+      create += specialArray;
+   };
+               
+   for ( i = 0; i < passwordLength; i++) {
+      var randomPass = Math.floor(Math.random() * create.length);
+      var choice = create.substring(randomPass, randomPass +1);
+      password += choice
+   };  return password
+ };
    
-   
-   
-   
-   
-   
+      
    function writePassword() {
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
-      
-      
+            
       passwordText.value = password;
-      
    }
    
 generateBtn.addEventListener("click", writePassword);
-
